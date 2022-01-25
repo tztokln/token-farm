ad9269482a8ae5cbf46ec2d481d5823e1afb4f29
elena <tztokln@gmail.com>
2022-01-25 15:03:42 -0500

    add tests

--- test/TokenFarm.js
+++ test/TokenFarm.js
@@ -1,40 +1,59 @@
 const { expect, assert } = require("chai");
+Web3 = require('web3')
 const hre = require("hardhat");
 
-require('chai')
-    .use(require('chai-as-promised'))
-    .should()
+// function tokens(n) {
+//     return Web3.utils.toWei(n, 'ether');
+// }
 
 describe("Token contract", function () {
 
-    let DappToken, dappToken, DaiToken, daiToken, owner, addr1, addr2, addrs;
+    let DappToken, dappToken, DaiToken, daiToken, TokenFarm, tokenFarm, owner, addr1, addr2, addrs;
 
     beforeEach(async function () {
         DappToken = await hre.ethers.getContractFactory("DappToken");
         dappToken = await DappToken.deploy();
+
         DaiToken = await hre.ethers.getContractFactory("DaiToken");
         daiToken = await DaiToken.deploy();
 
+        TokenFarm = await hre.ethers.getContractFactory("TokenFarm");
+        tokenFarm = await TokenFarm.deploy(dappToken.address, daiToken.address);
         [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
-        // console.log("HERE", owner, addr1, addr2)
+
+        // Transfer all Dapp tokens to farm (1 million)
+        await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+        // // Send tokens to investor
+        await  daiToken.transfer(addr1.address, '100000000000000000000')
 
     });
 
-    it('daiToken has a name', async () => {
-        const name = await daiToken.name()
-        assert.equal(name, 'Mock DAI Token')
-    })
-    it('dappToken has a name', async () => {
-        const name = await dappToken.name()
-        assert.equal(name, 'DApp Token')
+
+    describe('Mock DAI deployment', async () => {
+        it('has a nameee', async () => {
+            const name = await daiToken.name()
+            assert.equal(name, 'Mock DAI Token')
+        })
     })
 
-    it("Deployment should assign the total supply of tokens to the owner", async function () {
-        const ownerBalance = await dappToken.balanceOf(owner.address);
-        // for (const account of accounts) {
-        //   console.log(account.address,  " ==>" , await dappToken.balanceOf(account.address));
-        // }
-        expect(await dappToken.totalSupply()).to.equal(ownerBalance);
-    });
+    describe('Dapp Token deployment', async () => {
+        it('has a name', async () => {
+            const name = await dappToken.name()
+            assert.equal(name, 'DApp Token')
+        })
+    })
+
+    describe('Token Farm deployment', async () => {
+        it('has a name', async () => {
+            const name = await tokenFarm.name()
+            assert.equal(name, 'Dapp Token Farm')
+        })
+
+        it('contract has tokens', async () => {
+            let balance = await dappToken.balanceOf(tokenFarm.address)
+            assert.equal(balance.toString(), '1000000000000000000000000')
+        })
+    })
 
 });
