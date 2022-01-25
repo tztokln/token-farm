abd584ce3907b30278ca5ffa4a0828465645032b
elena <tztokln@gmail.com>
2022-01-24 22:18:32 -0500

    add more tests

--- test/TokenFarm.js
+++ test/TokenFarm.js
@@ -1,23 +1,40 @@
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 const hre = require("hardhat");
 
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    // const accounts = await ethers.getSigners();
-    const [owner] = await ethers.getSigners();
-    const DappToken = await hre.ethers.getContractFactory("DappToken");
-    const dappToken = await DappToken.deploy();
-    const ownerBalance = await dappToken.balanceOf(owner.address);
 
-        console.log("dappToken", await dappToken.name())
-    // for (const account of accounts) {
+    let DappToken, dappToken, DaiToken, daiToken, owner, addr1, addr2, addrs;
 
-    //     console.log("ownerBalance", await dappToken.totalSupply())
-    //   console.log(account.address,  " ==>" , await dappToken.balanceOf(account.address));
-    // }
+    beforeEach(async function () {
+        DappToken = await hre.ethers.getContractFactory("DappToken");
+        dappToken = await DappToken.deploy();
+        DaiToken = await hre.ethers.getContractFactory("DaiToken");
+        daiToken = await DaiToken.deploy();
 
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+        // console.log("HERE", owner, addr1, addr2)
 
-    expect(await dappToken.totalSupply()).to.equal(ownerBalance);
-  });
+    });
+
+    it('daiToken has a name', async () => {
+        const name = await daiToken.name()
+        assert.equal(name, 'Mock DAI Token')
+    })
+    it('dappToken has a name', async () => {
+        const name = await dappToken.name()
+        assert.equal(name, 'DApp Token')
+    })
+
+    it("Deployment should assign the total supply of tokens to the owner", async function () {
+        const ownerBalance = await dappToken.balanceOf(owner.address);
+        // for (const account of accounts) {
+        //   console.log(account.address,  " ==>" , await dappToken.balanceOf(account.address));
+        // }
+        expect(await dappToken.totalSupply()).to.equal(ownerBalance);
+    });
+
 });
